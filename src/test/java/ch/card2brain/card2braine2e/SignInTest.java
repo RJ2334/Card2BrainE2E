e8c71057b8ed6b5024086e646054328f460bcd0f
Alex Studer <alex@alexstuder.ch>
2023-01-31 19:19:15 +0100

    init

--- src/test/java/ch/card2brain/card2braine2e/SignInTest.java
+++ src/test/java/ch/card2brain/card2braine2e/SignInTest.java
@@ -2,35 +2,27 @@ package ch.card2brain.card2braine2e;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 
-
-public class Main {
+public class SignInTest {
     private static WebDriver webDriver;
 
-    public static void main(String[] args) {
+    @BeforeTest
+    public void init(){
         webDriver = new ChromeDriver();
-
-
         webDriver.get("http://167.86.76.193/home");
-        String title = webDriver.getTitle();
-        System.out.println(title);
-
-        //signIn();
-
-        login();
-        //logout();
-
-
-        webDriver.close();
-
     }
 
-
-    private static void signIn(){
-
+    @AfterTest
+    public void close(){
+        webDriver.close();
+    }
+    @Test
+    public void signInTest() {
         // click on button signin
         webDriver.findElement(By.cssSelector("button.btn.btn-secondary.ms-1")).click();
         // Fill in the users information
@@ -43,21 +35,5 @@ public class Main {
 
     }
 
-    private static void login(){
 
-        // click on button signin
-        webDriver.findElement(By.xpath("//*[@id=\"navbar\"]/form[1]/button[2]")).click();
-        webDriver.findElement(By.id("email")).sendKeys("Max@Musterman.ch");
-        webDriver.findElement(By.id("password")).sendKeys("Hallo1234");
-        //Submit
-        webDriver.findElement(By.xpath("/html/body/app-root/div/main/app-signup/div/form/div[3]/div[2]/button")).click();
-
-    }
-
-    private static void logout(){
-
-        //UserLogout
-        webDriver.findElement(By.xpath("//*[@id=\"navbar\"]/form[2]/button")).click();
-
-    }
 }
