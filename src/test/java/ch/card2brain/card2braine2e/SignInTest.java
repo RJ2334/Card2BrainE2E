d7fa83a41310261e5d5fc3197ac8d87f25b86f99
Alex Studer <alex@alexstuder.ch>
2023-01-31 23:45:37 +0100

    first tests

--- src/test/java/ch/card2brain/card2braine2e/SignInTest.java
+++ src/test/java/ch/card2brain/card2braine2e/SignInTest.java
@@ -1,7 +1,10 @@
 package ch.card2brain.card2braine2e;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Feature;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,7 +21,7 @@ public class SignInTest {
     public void init(){
         ChromeOptions options = new ChromeOptions();
         //options.setHeadless(true);
-        //options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         System.setProperty("webdriver.chrome.whitelistedIps", "167.86.76.193");
         webDriver = new ChromeDriver(options);
         webDriver.get("http://167.86.76.193/home");
@@ -40,7 +43,7 @@ public class SignInTest {
         webDriver.findElement(By.id("password")).sendKeys("Hallo1234");
         //Submit
         webDriver.findElement(By.xpath("/html/body/app-root/div/main/app-signup/div/form/div[3]/div[2]/button")).click();
-
+        //screenshot();
     }
     @Feature("LogIn")
     @Test(groups = {"SignInAndLogin"})
@@ -52,8 +55,12 @@ public class SignInTest {
         webDriver.findElement(By.cssSelector("#floatingPassword")).sendKeys("Hallo1234");
         //Submit
         webDriver.findElement(By.cssSelector("body > app-root > div > main > app-login > div > div > div > div > div > form > div.d-grid > button")).click();
-
+        //screenshot();
     }
 
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] screenshot() {
+        return ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+    }
 
 }
