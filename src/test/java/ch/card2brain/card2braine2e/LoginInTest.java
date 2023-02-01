00ccc96395783b331fde341d2d22b9a16c373220
Alex Studer <alex@alexstuder.ch>
2023-02-02 00:32:10 +0100

    new start

--- src/test/java/ch/card2brain/card2braine2e/LoginInTest.java
+++ src/test/java/ch/card2brain/card2braine2e/LoginInTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 
 
 @Feature("SignIn and Login")
-public class SignInTest {
+public class LoginInTest {
     private  WebDriver webDriver;
 
     @BeforeTest
@@ -31,21 +31,20 @@ public class SignInTest {
         webDriver.close();
         webDriver.quit();
     }
-    @Feature("SignIn")
+    @Feature("LogIn")
     @Test(groups = {"SignInAndLogin"})
-    public void signInTest() {
+    public void logInTest() {
         webDriver.get("http://167.86.76.193/home");
         // click on button signin
-        webDriver.findElement(By.xpath("/html/body/app-root/div/nav/div[2]/form[1]/button[1]")).click();
+        webDriver.findElement(By.cssSelector("#navbar > form:nth-child(1) >button.btn.btn-primary.ms-1")).click();
         // Fill in the users information
-        webDriver.findElement(By.id("firstName")).sendKeys("Max");
-        webDriver.findElement(By.id("lastName")).sendKeys("Musterman");
-        webDriver.findElement(By.id("email")).sendKeys("Max@Musterman.ch");
-        webDriver.findElement(By.id("password")).sendKeys("Hallo1234");
+        webDriver.findElement(By.cssSelector("#floatingInput")).sendKeys("Max@Musterman.ch");
+        webDriver.findElement(By.cssSelector("#floatingPassword")).sendKeys("Hallo1234");
         //Submit
-        webDriver.findElement(By.xpath("/html/body/app-root/div/main/app-signup/div/form/div[3]/div[2]/button")).click();
+        webDriver.findElement(By.cssSelector("body > app-root > div > main > app-login > div > div > div > div > div > form > div.d-grid > button")).click();
         //screenshot();
     }
+
     @Attachment(value = "Screenshot", type = "image/png")
     public byte[] screenshot() {
         return ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
